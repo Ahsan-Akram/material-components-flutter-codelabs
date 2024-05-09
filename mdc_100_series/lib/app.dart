c013cca10f62774d4448538e5b733849f50d381f
Ahsan-Akram <63956204+Ahsan-Akram@users.noreply.github.com>
2024-05-09 17:44:16 +0500

    Completed codelab 103

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -13,9 +13,13 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:shrine/supplemental/cut_corners_border.dart';
 
 import 'home.dart';
 import 'login.dart';
+import 'colors.dart';
+
+final ThemeData _kShrineTheme  = _buildShrineTheme();
 
 // TODO: Convert ShrineApp to stateful widget (104)
 class ShrineApp extends StatelessWidget {
@@ -34,11 +38,71 @@ class ShrineApp extends StatelessWidget {
         // TODO: Pass _currentCategory for frontLayer (104)
         // TODO: Change backLayer field value to CategoryMenuPage (104)
       },
-      // TODO: Customize the theme (103)
-      theme: ThemeData.light(useMaterial3: true),
+      theme: _kShrineTheme,
     );
   }
 }
+ThemeData _buildShrineTheme(){
+  final ThemeData base = ThemeData.light(useMaterial3: true);
 
-// TODO: Build a Shrine Theme (103)
-// TODO: Build a Shrine Text Theme (103)
+  return base.copyWith(
+      colorScheme: base.colorScheme.copyWith(
+          primary: kShrinePink100,
+          onPrimary: kShrineBrown900,
+          secondary: kShrineBrown900,
+          error: kShrineErrorRed
+      ),
+      textTheme: _buildShrineTextTheme(base.textTheme),
+      textSelectionTheme: const TextSelectionThemeData(
+        selectionColor: kShrinePink100,
+      ),
+      inputDecorationTheme: const InputDecorationTheme(
+        border: CutCornersBorder(),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 2.0,
+            color: kShrineBrown900,
+          ),
+        ),
+        floatingLabelStyle: TextStyle(color: kShrineBrown900),
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+            shape: const BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(7.0))),
+            foregroundColor: base.colorScheme.secondary),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            foregroundColor: kShrineBrown900,
+            backgroundColor: kShrinePink100,
+            elevation: 8.0,
+            shape: const BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(7.0)))
+        ),
+      )
+
+  );
+}
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+    headlineSmall: base.headlineSmall!.copyWith(
+      fontWeight: FontWeight.w500,
+    ),
+    titleLarge: base.titleLarge!.copyWith(
+      fontSize: 18.0,
+    ),
+    bodySmall: base.bodySmall!.copyWith(
+      fontWeight: FontWeight.w400,
+      fontSize: 14.0,
+    ),
+    bodyLarge: base.bodyLarge!.copyWith(
+      fontWeight: FontWeight.w500,
+      fontSize: 16.0,
+    ),
+  ).apply(
+    fontFamily: 'Rubik',
+    displayColor: kShrineBrown900,
+    bodyColor: kShrineBrown900,
+  );
+}
+
